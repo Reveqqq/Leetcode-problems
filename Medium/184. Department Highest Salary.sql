7de48d0ca3884c61973553a36d1cca237f12fe3e
Albert Zagibin <albert.zagibin@gmail.com>
2026-01-25 16:52:27 +1000

    problem 184

--- Medium/184. Department Highest Salary.sql
+++ Medium/184. Department Highest Salary.sql
@@ -1,11 +1,10 @@
 -- Write your PostgreSQL query statement below
 
 with top_salaries as (
-    select 
-        d.name as department,
-        e.name as employee,
-        e.salary as salary,
-        dense_rank() over(partition by d.id order by e.salary desc) as dr
+    select d.name as department
+        ,e.name as employee
+        ,e.salary
+        ,rank() over(partition by d.id order by e.salary desc) as r
     from Employee e
     join Department d on e.departmentId = d.id
 )
@@ -14,4 +13,4 @@ select
     employee as "Employee",
     salary as "Salary"
 from top_salaries
-where dr <= 3 
+where r = 1
