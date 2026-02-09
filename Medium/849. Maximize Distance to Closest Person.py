e3aa32bcfb5dc0d938c58df22f12d99ef5829228
Albert Zagibin <albert.zagibin@gmail.com>
2026-02-09 13:14:46 +1000

    refactor problem 849

--- Medium/849. Maximize Distance to Closest Person.py
+++ Medium/849. Maximize Distance to Closest Person.py
@@ -1,41 +1,17 @@
 from typing import List
 
 class Solution:
-    #1. set l to first empty seat, r = l
-    #2. find first r closet seat
-    #3. calc maxDist
-        # if l == 0, maxDist = r
-        # elif r == len(seats) - 1, maxDist = r - l + 1 # maybe this one after loop
-        # else maxDist = (r - l) // 2 + 1
-    #4. repaet 1-3 while l != len(seats) and r < len
-
     def maxDistToClosest(self, seats: List[int]) -> int:
-        def firstEmptySeat(l,seats):
-            while l < len(seats) and seats[l] == 1:
-                l += 1
-            return l
-
-        r = l = firstEmptySeat(0, seats)
-        maxDist = 1
-        N = len(seats)
-
-        while l < N:
-            while r < N and seats[r] == 0:
-                r += 1
+        maxDist, prev = 0, -1
+        for i, n in enumerate(seats):
+            if n:
+                dist = i if prev == -1 else (i - prev) // 2
+                maxDist = max(maxDist, dist)
+                prev = i
             
-            if l == 0:
-                maxDist = max(maxDist,r)
-            elif r == N and seats[r - 1] == 0:
-                maxDist = max(maxDist, r - l)
-            else:
-                c = (r - l)
-                if c % 2 == 0:
-                    maxDist = max(maxDist, c // 2)
-                else:    
-                    maxDist = max(maxDist, c // 2 + 1)
-
-            r = l = firstEmptySeat(r, seats)
-
+        if not seats[i]:
+            maxDist = max(maxDist, i - prev)
+        
         return maxDist
              
 
